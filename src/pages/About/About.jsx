4349e2592d8925ecbbbb5a327a530f7bcad3ffe7
ricci2511 <ricci2511@protonmail.com>
2022-07-17 23:28:57 +0200

    completed About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,99 @@
 import React from 'react';
 import AnimatedMain from '../../components/UI/AnimatedMain';
+import { FaReact } from 'react-icons/fa';
+import { SiTailwindcss, SiFramer } from 'react-icons/si';
+import { motion } from 'framer-motion';
 
 const About = () => {
-    return <AnimatedMain>ABOUT</AnimatedMain>;
+    return (
+        <AnimatedMain>
+            <h1 className="text-4xl">
+                About us or About{' '}
+                <strong className="border-b-4 border-b-purple">me?</strong>
+            </h1>
+            <section className="block max-w-[75ch] space-y-2 lg:text-lg">
+                <p>
+                    This website is built by me, Ricardo Christmann, online also
+                    known as{' '}
+                    <a
+                        href="https://github.com/ricci2511"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="font-bold">@ricci2511</i>.
+                    </a>
+                </p>
+                <p>
+                    Sadly you cannot purchase any of the cool items shown in
+                    this page. It's just fake data provided by{' '}
+                    <a
+                        href="https://fakestoreapi.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="font-bold"
+                    >
+                        Fake Store API
+                    </a>
+                    .
+                </p>
+                <p>
+                    Even though I got here after learning and reading through
+                    great docs/articles, the only one responsible for my success
+                    in this industry is <strong>me</strong>.
+                </p>
+                <div>
+                    <p>
+                        I built this project to show off some of the skills I
+                        learned recently and to pick up new ones. Built with:
+                    </p>
+                    <ul className="mt-6 space-y-3 px-8">
+                        <li>
+                            <div className="flex items-center gap-2">
+                                <FaReact color="#61DBFB" size={30} />
+                                <span>
+                                    <a
+                                        href="https://reactjs.org/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        React
+                                    </a>
+                                </span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="flex items-center gap-2">
+                                <SiTailwindcss color="#15C2B8" size={30} />
+                                <span>
+                                    <a
+                                        href="https://tailwindcss.com/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Tailwind CSS
+                                    </a>
+                                </span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="flex items-center gap-2">
+                                <SiFramer color="#B80889" size={30} />
+                                <span>
+                                    <a
+                                        href="https://www.framer.com/motion/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Framer Motion
+                                    </a>
+                                </span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </AnimatedMain>
+    );
 };
 
 export default About;
